72719e421f618bf1b3b438f9b120c6f9162d7977
Rishad <91536203+RishadPVM@users.noreply.github.com>
2022-03-06 15:30:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function but_click(val){
+    document.getElementById("display").value+=val
+}
+function clear_display(){
+    document.getElementById("display").value=""
+}
+function equalClick(){
+    var text= document.getElementById('display').value
+    var result= eval(text)
+    document.getElementById('display').value=result
+}
