b1b1a31b394b1de6b6a522d2e573c923afea9dc6
Rishad <91536203+RishadPVM@users.noreply.github.com>
2022-03-07 00:30:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function but_click(val){
     document.getElementById("display").value+=val
 }
 function clear_display(){
-    document.getElementById("display").value=""
+    document.getElementById("display").value="0"
 }
 function equalClick(){
     var text= document.getElementById('display').value
